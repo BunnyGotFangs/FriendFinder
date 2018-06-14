540b60a89c6fbb0edc41f40012d45add1abc2a1e
BunnygotFangs <kimberly@thehilts.com>
2018-06-13 22:45:16 -0500

    upload of homework assignment to github

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//this is the file for holding all the new peeps added
+
+var peeps = [
+    {
+      Name: 'Red Barron',
+      Pic: 'https://www.pinterest.com/pin/364228688599256038',
+      Ratings: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = peeps;
